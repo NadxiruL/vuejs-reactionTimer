2ed768f790ff5a731e9b7af285a5b8de1a722b6d
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2022-04-09 22:33:38 +0800

    show block

---
+++ src/components/Block.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="block">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+
+<style>
+.block {
+  width: 300px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px;
+  margin: 40px auto;
+}
+</style>
