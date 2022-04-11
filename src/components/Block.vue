582b6f88b6d7e671e234ba01adb6638a2ff72fb8
NadxiruL <66035094+NadxiruL@users.noreply.github.com>
2022-04-11 17:43:10 +0800

    lifecycle hooks

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="block">click me</div>
+  <div class="block" v-if="showBlock">click me</div>
 </template>
 
 <script>
 export default {
   props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  //fire only when mounted to dom component.
+  mounted() {
+    setTimeout(() => {
+      this.showBlock = true;
+      console.log(this.delay);
+    }, this.delay);
+  },
+  //fire when any data inside our dom is updated.
+  updated() {
+    console.log("test");
+  },
+  unmounted() {
+    // todo here..
+  },
 };
 </script>
 
